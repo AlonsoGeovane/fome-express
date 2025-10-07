aad2da99336d1d1dc5ee56b5bdd0501958ccaabc
Geovane Alonso <geovane.halonso.2005@gmail.com>
2025-10-06 22:00:00 -0300

    Começando

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+export const HomePage = () => {
+    return (
+        <main className="min-h-dvh grid place-items-center bg-slate-50">
+            <h1 className="text-3xl font-bold text-slate-800">
+                Fome Express
+            </h1>
+
+            <div clasName="fixed top-0 left-0">
+                <ul className="fixed left-[100px] top-[11px]">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+        
+        </div>
+        </main >
+    );
+}
