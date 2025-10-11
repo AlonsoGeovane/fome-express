16d4074573be77382dc5d50031935e9d34b0d4cc
Geovane Alonso <geovane.halonso.2005@gmail.com>
2025-10-10 21:52:28 -0300

    Finalizado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,13 @@
 export const HomePage = () => {
     return (
-        <main className="min-h-dvh grid place-items-center bg-slate-50">
-            <h1 className="text-3xl font-bold text-slate-800">
-                Fome Express
-            </h1>
+        <main className="min-h-dvh grid text-center bg-gray-900">
+            <header className="text-2xl font-bold text-white">Fome Express</header>
 
-            <div clasName="fixed top-0 left-0">
-                <ul className="fixed left-[100px] top-[11px]">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
-        
-        </div>
+            <div className="bg-white shadow-md rounded-lg p-4 max-w-[350px]">
+                <h2 className="text-xl font-bold mb-2 ">1</h2>
+                <p className="text-gray-700">1</p>
+            </div>
+
         </main >
     );
 }
